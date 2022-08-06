7f9b4280e161910671ce6c531103e0f02ecd615b
Omar Mokhtar <omar.shatat95@gmail.com>
2022-08-07 01:30:25 +0200

    Added learned stuff as a reference.

---
+++ learned-stuff.py
@@ -0,0 +1,10 @@
+# len() --> length of a string
+# strip() --> removes ALL spaces or a specific character if specified as an argument
+# lstrip() --> same as strip() but from the left side only
+# rstrip() --> same as rstrip() but from the right side only
+# title() --> captializes any start of a string and any character next to a number
+# capitalize() --> captializes any start of a string
+# zfill() --> fills a given numeric string to the given argument
+# upper() --> captializes a given string
+# lower() --> makes the given string small
+# --------------------------------------------------------------------------------
