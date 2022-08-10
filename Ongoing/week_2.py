88d21d1f61e21cedfc42b9128f04f7d67a02d751
Omar Mokhtar <omar.shatat95@gmail.com>
2022-08-10 16:03:00 +0200

    1:4 Week 2 Assignments

---
+++ Ongoing/week_2.py
@@ -0,0 +1,22 @@
+# Assignment #1
+# name = "Omar"
+# age = "20"
+# country = "Egypt"
+# print("Hello \'"+name+"\', How You Doing \\ \"\"\" Your Age is \" " +
+#     age+"\"\" + And Your Country Is: "+country)
+# ----------------------------------------------------------------------------------
+# Assignment #2
+# name = "Omar"
+# age = "20"
+# country = "Egypt"
+# print("\"Hello \'"+name+"\', How You Doing \\\n" +
+# "\"\"\" Your Age Is \""+age+"\"\" +\n" + "And Your Country Is: "+country)
+# ----------------------------------------------------------------------------------
+# Assignment #3
+# name = 'Elzero'
+# print("Second Letter Is \""+name[1]+"\"\n"+"Third Letter Is \"" +
+#      name[2]+"\"\n"+"Last Letter Is \""+name[-1]+"\"\n")
+# ----------------------------------------------------------------------------------
+# Assignment #4
+name = 'Elzero'
+print(name[1:4]+"\n"+name[::2]+"\n"+name[-2:-7:-2])
