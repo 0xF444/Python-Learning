3c7f2e79141d461d5e5f80694c7d102c93dc6e21
Omar Mokhtar <omar.shatat95@gmail.com>
2022-08-11 16:58:47 +0200

    Strings finished

---
+++ Finished/week_2.py
@@ -0,0 +1,68 @@
+# Assignment #1
+# name = "Omar"
+# age = "20"
+# country = "Egypt"
+# print("Hello \'"+name+"\', How You Doing \\ \"\"\" Your Age is \" " +
+#     age+"\"\" + And Your Country Is: "+country)
+# ----------------------------------------------------------------------------------
+# Assignment #2
+# name = "Omar"
+# age = "20"
+# country = "Egypt"
+# print("\"Hello \'"+name+"\', How You Doing \\\n" +
+# "\"\"\" Your Age Is \""+age+"\"\" +\n" + "And Your Country Is: "+country)
+# ----------------------------------------------------------------------------------
+# Assignment #3
+# name = 'Elzero'
+# print("Second Letter Is \""+name[1]+"\"\n"+"Third Letter Is \"" +
+#      name[2]+"\"\n"+"Last Letter Is \""+name[-1]+"\"\n")
+# ----------------------------------------------------------------------------------
+# Assignment #4
+#name = 'Elzero'
+# print(name[1:4]+"\n"+name[::2]+"\n"+name[-2:-7:-2])
+# ----------------------------------------------------------------------------------
+# Assignment #5
+# name = "#@#@Elzero#@#@"
+# print(name.strip("#@"))
+# ----------------------------------------------------------------------------------
+# Assignment #6
+# num1 = "9"
+# num2 = "15"
+# num3 = "130"
+# num4 = "950"
+# num5 = "1500"
+# print(num1.zfill(4)+"\n"+num2.zfill(4)+"\n" +
+#       num3.zfill(4)+"\n"+num4.zfill(4)+"\n"+num5.zfill(4))
+# ----------------------------------------------------------------------------------
+# Assignment #7
+# name1 = "Osama"
+# name2 = "Osama_Elzero"
+# print(name1.rjust(20, "@")+"\n"+name2.rjust(20, "@"))
+# ----------------------------------------------------------------------------------
+# Assignment #8
+# name1 = "OSamA"
+# name2 = "osaMA"
+# print(name1.swapcase()+"\n"+name2.swapcase())
+# ----------------------------------------------------------------------------------
+# Assignment #9
+# msg = "I Love Python And Although Love Elzero Web School"
+# print(msg.count("Love"))
+# ----------------------------------------------------------------------------------
+# Assignment #10
+# n = "Elzero"
+# print(n.index("z"))
+# ----------------------------------------------------------------------------------
+# Assignment #11
+# msg = "I <3 Python And Although <3 Elzero Web School"
+# print(msg.replace("<3", "Love", 1))
+# ----------------------------------------------------------------------------------
+# Assignment #12
+# msg = "I <3 Python And Although <3 Elzero Web School"
+# print(msg.replace("<3", "Love"))
+# ----------------------------------------------------------------------------------
+# Assignment #13
+# name = "Omar"
+# age = 20
+# country = "Egypt"
+# print(f"My name is {name}, And My Age Is {age}, And My Country Is {country}")
+# ----------------------------------------------------------------------------------
