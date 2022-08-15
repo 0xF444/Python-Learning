3c3c47bef62a3dce42fa674a775ec233739a1bdc
Omar Mokhtar <omar.shatat95@gmail.com>
2022-08-15 18:23:39 +0200

    End of lists assignment

--- Ongoing/week_3.py
+++ Ongoing/week_3.py
@@ -46,4 +46,49 @@
 # print(friends[1:5:2])
 # ----------------------------------------------------------------------------------
 # Assignment #3
-friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+# friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+# print(friends[1:4])
+# print(friends[-2::1]) # note: starts at -2 and TAKES THE END WITH IT, UNLIKE PUTTING -1 AS AN END.
+# ----------------------------------------------------------------------------------
+# Assignment #4
+# friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+# friends[-2] = "Elzero"
+# friends[-1] = "Elzero"
+# print(friends)
+# ----------------------------------------------------------------------------------
+# Assignment #5
+# friends = ["Osama", "Ahmed", "Sayed"]
+# friends.insert(0, "Nasser")
+# friends.append("Salem")
+# print(friends)
+# ----------------------------------------------------------------------------------
+# Assignment #6
+# friends = ['Nasser', 'Osama', 'Ahmed', 'Sayed', 'Salem']
+# friends.pop(0) # changes the iterable
+# friends.pop(0) # since 'Osama' is the new zero index element
+# print(friends)
+# friends.pop(-1)
+# print(friends)
+# ----------------------------------------------------------------------------------
+# Assignment #7
+# friends = ["Ahmed", "Salah"]
+# employees = ["Samah", "Eman"]
+# school = ["Ramy", "Shady"]
+# friends.extend(employees)
+# friends.extend(school)
+# print(friends)
+# ----------------------------------------------------------------------------------
+# Assignment #8
+# friends = ["Ahmed", "Sayed", "Samah", "Eman", "Ramy", "Shady"]
+# friends.sort()
+# print(friends)
+# friends.sort(reverse=True)
+# print(friends)
+# ----------------------------------------------------------------------------------
+# Assignment #9
+# friends = ["Ahmed", "Sayed", "Samah", "Eman", "Ramy", "Shady"]
+# print(len(friends))
+# ----------------------------------------------------------------------------------
+technologies = ["Html", "CSS", "JS", "Python", ["Django", "Flask", "Web"]]
+print(technologies[-1][0])
+print(technologies[-1][2])
