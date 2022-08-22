2351eb1eca1325ac854d09ed975d200e0ab01f10
Omar Mokhtar <omar.shatat95@gmail.com>
2022-08-22 13:37:10 +0200

    New learned stuff file

---
+++ log.py
@@ -0,0 +1,43 @@
+#                                         BOOLEAN
+# True or False
+# bool() returns true or false true if anything and false if empty container or false itself.
+# --------------
+# bool operators
+# "and" ANDs the two conditions
+# "or" ORs the two conditions
+# "not" reverses
+# they're the same as logic statements which we know
+# --------------
+# Assignment operators
+# they "assign" a value to a variable
+# "=" normally assigns a value to a variable
+# "+=" adds the old value of a variable to some different variable and assigns it to the variable
+# "-=" same but subtracts
+# "*=" same but multiplies
+# "/=" same but divides
+# "**=" same but powers (exponential)
+# "%=" same but takes the modulo
+# "//=" same but floor divides
+# --------------
+# Comparison operators
+# "==" equals
+# "!=" not equal
+# ">" larger than
+# "<" less than
+# ">=" larger or equal
+# "<=" smaller or equal
+# ""
+# --------------
+# Type Conversion
+# str() --> to string
+# tuple() --> to tuple
+# list() --> to list
+# set() --> to set
+# dict() --> to dict (cannot convert a string to a dict)
+# as for tuples, use nested tuples (key,value)
+# as for lists, use nested lists [key,value]
+# as for sets, unhashable (cannot convert) as you cannot get values from a set
+# --------------
+# input() --> stops executing until an input is entered from stdin
+# chain method --> you can chain methods by dots normally
+# --------------
