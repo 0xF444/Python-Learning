f8d8c57345a0733cd7dd99b22cf8c09c931ee428
virtuosity <omar.shatat95@gmail.com>
2022-08-29 17:58:32 +0200

    Stopped till assignment #3

---
+++ Ongoing/week_7.py
@@ -0,0 +1,39 @@
+# Assignment #1
+# num = int(input("Please enter a positive number.\n"))
+# while num<=0:
+#     num = int(input(f"Number 0 {num} is not larger than 0.\n"))
+# else:
+#     print("Correct entry.")
+# count = 0
+# while num>1:
+#     num-=1
+#     if(num==6):
+#         continue
+#     print(num)
+#     count +=1
+# else:
+#     print(f"All numbers have beeen printed out succesfully.\nA total of {count} numbers have been printed.")
+# ################################
+# Assignment #2
+# counter = 5
+# friends = []
+# while counter != 0:
+#     name = input(f"Please enter your friend's name, {counter} entries left.\n")
+#     friends.append(name)
+#     counter -=1
+# cnt2=0
+# ignored = 0
+# while cnt2<5:
+#     if friends[cnt2].islower():
+#         cnt2 +=1
+#         ignored +=1
+#         continue
+#     else:
+#         print(friends[cnt2])
+#     cnt2+=1
+# print(f"Your friends have been successfully printed, and {ignored} have been ignored.\n")
+# ################################
+# Assignment #3
+skills = ["HTML", "CSS", "JavaScript", "PHP", "Python"]
+while skills:
+    print(skills)
