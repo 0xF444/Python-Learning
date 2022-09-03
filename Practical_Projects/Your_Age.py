ebf957d91fdc1dd47a80e7a0ad5bcebf143f045f
Omar Mokhtar <omar.shatat95@gmail.com>
2022-09-03 18:49:02 +0200

    Removed unneeded files.

--- Practical_Projects/Your_Age.py
+++
@@ -1,9 +0,0 @@
-age = int(input("What\'s your age?\n").strip())
-months = age*12
-weeks = months*4
-days = age*365
-hours = days * 24
-minutes = hours * 60
-seconds = minutes * 60
-print(
-    f"You have lived {months} months\n{days} days\n{hours} hours \n{minutes} minutes\n{seconds} seconds")
