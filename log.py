f0c5c525ed17a5efbadb88b87d17070c04e88fed
virtuosity <omar.shatat95@gmail.com>
2022-09-05 16:38:59 +0200

    Changed a couple of files

--- log.py
+++ log.py
@@ -7,11 +7,30 @@
 # ---------------------------------------------------------------------------------------
 # open() ==> opens a file's path and returns it in a stream, requires the file and the mode
 # import os is a module that contains extra handy methods and functions
-# os.cwd: gets the current working directory
+# os.getcwd(): gets the current working directory
 # os.path.abspath(__file__) returns the absolute directory of the CURRENT FILE WE'RE IN or program
 # os.path.dirname(file) returns the directory in which the file is contained
 # os.chdir() changes your current working directory
+# -------
+# READ "r"
 # we can add "r" <rowed string> to the string so it can avoid any escape sequences or commands within the string
-# the read attribute gives the data of the data object NOT its content
+# the read attribute gives the data of the file data object NOT its content
 # .name: prints the name, .mode: prints the mode, .encoding prints the encoding
-# .read() can take the number of bytes (character) needed, default value is -1 (which is the entire file)
+# .read() returns the content of the file, can take the number of bytes (characters) needed, default value is -1 (which is the entire file)
+# .readline() reads a line, can take specific line characters
+# .readlines() returns all the lines in a list ending with a "\n", you can limit what comes back by an attribute
+# .startswith() bool function that checks if the line starts with the argument
+# .close() closes the file
+# -------
+# WRITE "w" and APPEND "a"
+# .write() is used to write content to the file, overwriting it
+# .writelines(<group iterable>) can take a tuple or a list etc, it writes them to the file
+# append basically writes but doesn't overwrite
+# it also takes into consideration the writing cursor
+# .truncate() takes a number of bytes and then returns those bytes and overwrites
+# .tell() tells you where the cursor is
+#! IMPORTANT NOTE: THE LINE BREAK CODE IN LINUX IS JUST ONE CHARACTER BUT IN WINDOWS IT'S TWO.
+# .seek() sends the cursor to a given offset
+# we need the os module for file manipulation 
+# os.remove(<filepath>) removes the file 
+
