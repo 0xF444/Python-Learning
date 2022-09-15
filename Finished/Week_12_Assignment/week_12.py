1a5b4ef4e3f1803793a5013a902f7818670cd834
virtuosity <omar.shatat95@gmail.com>
2022-09-15 23:12:22 +0200

    Finished Week 12

--- Finished/Week_12_Assignment/week_12.py
+++ Finished/Week_12_Assignment/week_12.py
@@ -29,6 +29,8 @@ final_string3 = ''.join(my_data)
 print(final_string3)
 print("="*150)
 # Assignment #3 -> Sheet 1
+from curses.ascii import islower
+from multiprocessing.sharedctypes import Value
 from PIL import Image
 img = Image.open(r"C:\Users\magic group\Desktop\Python-Learning\Ongoing\Week_12_Assignment\elzero-pillow.png")
 newimg1 = img.crop((400,0,800,400)) # don't forget about how points and vectors move, that's how you'll understand ;)
@@ -50,6 +52,7 @@ def say_hello_to(name) -> str:
     print(f"Hello {name}!")
 print(say_hello_to.__doc__)
 print(help(say_hello_to))
+print("="*150)
 # -----------------------
 # Assignment #5 -> Sheet 1
 '''This module contains a function that greets people
@@ -62,6 +65,43 @@ def say_hello(some_peoples) -> list:
     for some_one in some_peoples:
         print(f"Hello {some_one}.")
 say_hello(my_Friends)
+print("="*150)
 # It has been linted and scored 10/10 in a seperate file.
 # -----------------------
+# Assignment #1 -> Sheet 2
+NUM = input("Add Your Number: ")
+if len(NUM) != 1:
+    raise IndexError("Only one character allowed.")
+elif NUM == "0":
+    raise ValueError("Number must be larger than 0.")
+elif NUM.isalpha():
+    raise Exception("Only numbers allowed")
+else:
+    print("####################")
+    print("The number is",NUM)
+    print("####################")
+print("="*150)
+# -----------------------
+# Assignment #2 -> Sheet 2
+try:
+    LETTER = input("Add a Letter From A to Z: ")
+    if len(LETTER)!=1:
+        raise ValueError
+    elif LETTER.islower() or LETTER.isnumeric():
+        raise TypeError
+except ValueError:
+    print("You must only print one character")
+except TypeError:
+    print("The letter is not in A - Z")
+else:
+    print(f"You Typed {LETTER}")
+print("="*150)
+# -----------------------
+# Assignment #3 -> Sheet 2
+def calculate(num1, num2) -> int:
+    return num1 + num2
+print(calculate(20, 30))
+print("="*150)
+# -----------------------
+
 
