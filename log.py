6b1bd583e1086c19926af62d0d8ae3a8846002a7
Omar Mokhtar <omar.shatat95@gmail.com>
2022-09-17 00:15:20 +0200

    Regex intro

---
+++ log.py
@@ -0,0 +1,4 @@
+# Regular expressions are expressions that define a search pattern: remember the grep command in ubuntu?
+# https://pythex.org to test our expressions
+# https://www.debuggex.com/cheatsheet/regex/python for char sets
+# mostly used in validations
