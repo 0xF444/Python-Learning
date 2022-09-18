086ccc6d7b4ea1283810594eeed83b8ff8558de4
Omar Mokhtar <omar.shatat95@gmail.com>
2022-09-18 05:42:33 +0200

    added notes till part 6

--- log.py
+++ log.py
@@ -2,3 +2,14 @@
 # https://pythex.org to test our expressions
 # https://www.debuggex.com/cheatsheet/regex/python for char sets
 # mostly used in validations
+# Regex explaination: https://regex101.com
+# search() --> searchs a string for a match and returns the first match only
+# findall() --> returns a list of all matches and returns an empty list if there are no matches
+# to use regex: import re
+# re.span() returns the position of the match
+# re.string() returns the string that you're searching within
+# re.group() returns the group that has the matches
+
+import re
+my_string = re.findall("[A-Z]", "OsamaElzero")
+print(my_string)
