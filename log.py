b5e67f453c4d7ad4c415d1922954a0c041415810
Omar Mokhtar <omar.shatat95@gmail.com>
2022-09-19 03:11:23 +0200

    Finished Regex notes

--- log.py
+++ log.py
@@ -2,14 +2,17 @@
 # https://pythex.org to test our expressions
 # https://www.debuggex.com/cheatsheet/regex/python for char sets
 # mostly used in validations
-# Regex explaination: https://regex101.com
+# TODO Regex explaination: https://regex101.com
 # search() --> searchs a string for a match and returns the first match only
 # findall() --> returns a list of all matches and returns an empty list if there are no matches
 # to use regex: import re
 # re.span() returns the position of the match
 # re.string() returns the string that you're searching within
 # re.group() returns the group that has the matches
-
-import re
-my_string = re.findall("[A-Z]", "OsamaElzero")
-print(my_string)
+# re.split() returns a list that contains the string with each match splitted, can also take a maxsplit
+# re.sub() replaces each match with anything that we want, can also take a max replace
+#! each of these commands take (pattern,string,max,flags)
+# groups in regular expressions are pretty important
+# re.group(<group number>) returns the group that got matched
+# re.groups() returns all the groups that got matched
+# * re flags: IGNORECASE (case insensitive), VERBOSE (allows commenting within the regular expression), DOTALL (since "." matches everything but the newline, this allows to match the newline as well), MULTILINE (default is it searchs from the start of the line the end of the line, this allows to search all lines)
